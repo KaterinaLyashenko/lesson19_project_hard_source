a27afc7e521a2c5d484dfffa7aacd6274905a044
KaterinaLyashenko <ekl18062003@yandex.ru>
2022-08-28 22:00:44 +0300

    проект создан

---
+++ views/users.py
@@ -0,0 +1,34 @@
+from flask import request
+from flask_restx import Namespace, Resource
+
+from dao.model.user import UserSchema
+from implemented import user_service
+
+user_ns = Namespace("user")
+
+@user_ns.route('/')
+class UsersView(Resource):
+    def get(self):
+        user = user_service.get_all()
+        return UserSchema(many=True).dump(user), 200
+
+    def post(self):
+        req_json = request.json
+        user = user_service.create(req_json)
+        return "", 201, {"location": f"/movies/{user.id}"}
+
+
+@user_ns.route('/<int:uid>')
+class UsersView(Resource):
+    def get(self, uid):
+        user = user_service.get_one(uid)
+        return UserSchema(many=True).dump(user), 200
+
+    def put(self, uid):
+        req_json = request.json
+        if "id" not in req_json:
+            req_json["id"] = uid
+        user_service.update(req_json)
+
+        return "", 204
+
